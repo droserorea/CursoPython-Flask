dd526dcb4fff1cac3bf9d1dc01395ecd963f2c48
droserorea <droserorea@gmail.com>
2022-02-18 18:07:02 -0500

    end intro

--- intro-flask/holaMundo.py
+++ intro-flask/holaMundo.py
@@ -39,4 +39,13 @@ def  home():
 
 @app.route('/crear', methods=['GET','POST'])
 def crear():
+    if request.method=="POST":
+        cedula= request.form['cedula']
+        nombre= request.form['nombre']
+        fecha= request.form['fecha']
+        sql= "INSERT INTO public.estudiantes(cedula, fecha, nombre) VALUES (%s,%s,%s)"
+        values= (cedula,fecha,nombre)
+        cursor.execute(sql,values)
+        conexion.commit()
+        return redirect(url_for('home'))
     return render_template ('crear.html')
